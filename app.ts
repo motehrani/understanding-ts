ea5f1c7d352b8087c3135158b38b5b80363bb6ac
htehrani98 <h.tehrani1377@gmail.com>
2022-10-11 22:07:00 +0330

    15

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("your code goes here..");
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
