d4dcd81a612aa7014859e8ce6d1d63274d50523e
htehrani98 <h.tehrani1377@gmail.com>
2022-10-17 21:08:17 +0330

    22 Wrap Up

--- app.ts
+++ app.ts
@@ -7,3 +7,12 @@ userInput = "max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number) {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
+
+// line 12??
+
+generateError("an error occurred!", 500);
