7dba47e74e0843801e7e338b70dbf7798eee6ff6
htehrani98 <h.tehrani1377@gmail.com>
2022-10-20 11:31:03 +0330

    E3 Compiling the Entire Project _ Multiple Files

--- app.ts
+++ app.ts
@@ -5,3 +5,7 @@ console.log(userName);
 // tsc app.ts -w
 // or
 // tsc app.ts --watch
+
+// but the question is what if we have lot of files...
+// in that position we got to do tsc command for each fucking file??
+// so than we should say tsc --init in the terminal and than say just tsc
